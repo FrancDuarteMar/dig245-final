812806e4887aec1b3b3802c111c4fc96ec3dd5f9
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-11-27 17:45:36 -0500

    updated stable images through 100 and fixed duplicate image selection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,302 +1,161 @@
-// async function getPreData() {
-
-//     var tempVar;
-    
-//     var requestOptions = {
-//         method: 'GET',
-//         redirect: 'follow'
-//       };
-      
-//     //   await fetch("https://datasets-server.huggingface.co/first-rows?dataset=laion%2Flaion2B-en&config=laion--laion2B-en&split=train")
-//         await fetch("https://datasets-server.huggingface.co/first-rows?dataset=ChristophSchuhmann%2Fimproved_aesthetics_6.5plus&config=ChristophSchuhmann--improved_aesthetics_6.5plus&split=train")
-//         .then(response => {
-//             return response.json();
-//             })
-//         .then(data => {
-//             tempVar = data;
-//         })
-//         .catch(error => console.log('error', error));
-//         return tempVar;
-// }
-
-
 var correctAnswer;
-
-// async function getAbstFile(){
-//     return `<img src= "${"./assets/img/abst/"+Math.floor(Math.random() * 22)+".jpg"}"width="300" height="300">`
-    
-// }
+var allFiles;
+var running = true;
 
 
-// function findValidImg(dataset){
-//     var keys = Object.keys(dataset);
-//     var randomProperty = dataset[keys[ keys.length * Math.random() << 0]]
-//     // console.log("random")
-//     // console.log(randomProperty['row']['URL'])
-//     // if(randomProperty["row"])
-    
-//     // };
-//     // console.log(randomProperty)
-//     return randomProperty['row']['URL']
-
-    
-// }
-
-
-
-// async function loadNewSet(){
-//     $(".preMadeImg").ready(async function (){
-
-//         // let respon = await getPreData();
-//         // console.log("DONE")
-//         // console.log(Object.keys(respon))
-//         // console.log(respon['rows'])
-//         // $(".test").attr("src",findValidImg(respon['rows']))
-//         $(".test").attr("src",'./assets/img/laion/'+Math.floor(1+ Math.random() * 82)+'.jpg')
-
-
-
-//         // $(".test").attr("src",'./assets/img/abst/'+Math.floor(1+ Math.random() * 22)+'.jpg')
-//         // console.log(location)
-
-        
-//         // console.log(findValidImg(respon['rows']))
-//         }
-//     )
-
-// }
-
-// $(document).on('click', 'input[type="checkbox"]', function() {      
-//     $('input[type="checkbox"]').not(this).prop('checked', false);      
-// });
-
-$(document).ready(function () {
-    let getFile = Math.floor(1+ Math.random() * 82)
-
-    let difficulty = Math.floor(1+ Math.random() * 101)
-        // console.log("Difficulty: "+difficulty)
-        if(difficulty <= 16){
-            $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-            $(".genImg").attr("src",'./assets/img/stable-5/'+getFile +'.jpg')
-            $("#diffLevel").html("Current difficulty: 1")
+function getNewData(fileToLookAt){
+        $(':button').prop('disabled', false);
+        $(':button').css("border","3px solid black");   
+        if(Math.floor(Math.random() * 101) > 50) {
+            $('#top').hide();
+            $('#bottom').show();
+            correctAnswer = "bottom";
         }
-        else if(16 < difficulty && difficulty<= 32){
-            $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-            $(".genImg").attr("src",'./assets/img/stable-25/'+getFile +'.jpg')
-            $("#diffLevel").html("Current difficulty: 2")
-        }
-                    
-        else if(32 < difficulty && difficulty <= 48){
-            $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-            $(".genImg").attr("src",'./assets/img/stable-50/'+getFile +'.jpg')
-            $("#diffLevel").html("Current difficulty: 3")
-
-                }    
-        else if(48 < difficulty && difficulty <= 64){
-     
-            $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-            $(".genImg").attr("src",'./assets/img/stable-100/'+getFile +'.jpg')
-            $("#diffLevel").html("Current difficulty: 4")
-
-        }
-        else if(64 < difficulty && difficulty <= 80){
-            $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-            $(".genImg").attr("src",'./assets/img/stable-500/'+getFile +'.jpg')
-            $("#diffLevel").html("Current difficulty: 5")
-
-            }
         else{
-     
-            $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-            $(".genImg").attr("src",'./assets/img/stable-1000/'+getFile +'.jpg')
-            $("#diffLevel").html("Current difficulty: 6")
+            $('#bottom').hide();
+            $('#top').show();
+            correctAnswer = "top";
+        }  
+        
+        let getFile = fileToLookAt["image"]
+        let imgPrompt = fileToLookAt['prompt']
+        
+        let difficulty = Math.floor(1+ Math.random() * 64)
+            if(difficulty <= 16){
+                $(".humImg").attr("src",'./assets/img/laion/'+getFile)
+                $(".genImg").attr("src",'./assets/img/stable-5/'+getFile)
+                $("#diffLevel").html("Current difficulty: 1")
+            }
+            else if(16 < difficulty && difficulty<= 32){
+                $(".humImg").attr("src",'./assets/img/laion/'+getFile )
+                $(".genImg").attr("src",'./assets/img/stable-25/'+getFile)
+                $("#diffLevel").html("Current difficulty: 2")
+            }
+                        
+            else if(32 < difficulty && difficulty <= 48){
+                $(".humImg").attr("src",'./assets/img/laion/'+getFile)
+                $(".genImg").attr("src",'./assets/img/stable-50/'+getFile)
+                $("#diffLevel").html("Current difficulty: 3")
+
+                    }    
+            else if(48 < difficulty && difficulty <= 64){
+        
+                $(".humImg").attr("src",'./assets/img/laion/'+getFile)
+                $(".genImg").attr("src",'./assets/img/stable-100/'+getFile)
+                $("#diffLevel").html("Current difficulty: 4")
 
             }
+            else if(64 < difficulty && difficulty <= 80){
+                $(".humImg").attr("src",'./assets/img/laion/'+getFile)
+                $(".genImg").attr("src",'./assets/img/stable-500/'+getFile)
+                $("#diffLevel").html("Current difficulty: 5")
+
+                }
+            else{
         
+                $(".humImg").attr("src",'./assets/img/laion/'+getFile)
+                $(".genImg").attr("src",'./assets/img/stable-1000/'+getFile)
+                $("#diffLevel").html("Current difficulty: 6")
+
+                }
             
-    if(Math.floor(Math.random() * 101) > 50) {
-        $('#top').hide();
-        correctAnswer = "bottom";
+                
+
+    
+        // jQuery(':button').click(function () {
+        jQuery(':button').one('click',function(){
+            $(':button').prop('disabled', true);
+            if (this.id == 'genOpt1') {
+                if(correctAnswer = "top"){
+                    $(this).css("border","10px solid red");      
+                }
+                else{
+                    $(this).css("border","10px solid red");    
+                }
+            }
+            else if (this.id == 'genOpt2') {
+                if(correctAnswer = "bottom"){
+                    $(this).css("border","10px solid red");      
+                }
+                else{
+                    $(this).css("border","10px solid red");    
+                }
+            }
+
+            else if (this.id == 'realOpt1') {
+                if(correctAnswer = "top"){
+                    $(this).css("border","10px solid green");      
+                }
+                else{
+                    $(this).css("border","10px solid red");    
+                }
+            }
+            else if (this.id == 'realOpt2') {
+                if(correctAnswer = "bottom"){
+                    $(this).css("border","10px solid green");      
+                }
+                else{
+                    $(this).css("border","10px solid red");    
+                }
+            }
+            else if(this.id == "continue"){
+                // location.reload();
+                getRandFile(allFiles)
+
+
+            }
+
+            $('#continue').prop('disabled', false);
+
+            
+
+        });
+
+        
+}
+
+function getRandFile(files){
+    console.log(Object.keys(files).length)
+    
+    var randomFile = function (obj) {
+        var keys = Object.keys(obj);
+        var pos = keys[keys.length * Math.random() << 0]
+        var objToReturn = obj[pos]
+        delete obj[pos]
+        return objToReturn;
+    };
+    let currentFile = randomFile(files)
+    if(Object.keys(files).length < 1){
+        running = false
+    }
+    console.log(currentFile)
+    if(running){
+        getNewData(currentFile)
     }
     else{
-        $('#bottom').hide();
-        correctAnswer = "top";
-    }  
+        alert("Thats all the images!")
+    }
+}
 
-    // $('#isAgeSelected').click(function() {
-    //     $(".test").attr("src",'./assets/img/abst/'+Math.floor(Math.random() * 22)+'.jpg')
-    // });
-    
-    
+function initialize(jsonData){   
+    $(document).ready(function () {
 
-
-
-    // if(document.getElementById(abstImg).checked){
-    //     // getAbstFile
-        // ${"./assets/img/abst/"+Math.floor(Math.random() * 22)
-        // $(".test").attr("src",'./assets/img/abst/'+Math.floor(Math.random() * 22)+'.jpg')
-    // }
-    // else{
-        // let getFile = Math.floor(1+ Math.random() * 82)
-        // $(".test").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-
-    // }
-
-    
-    // loadNewSet();
-
-    // $('#difficulty').on('change', function () {
-    //     let getFile = Math.floor(1+ Math.random() * 82)
-    //     switch ($('#difficulty :selected').val()) {
-    //         case "1":
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-5/'+getFile +'.jpg')
-    //             break;
-    //         case "2": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-25/'+getFile +'.jpg')
-    //             break
-    //         case "3": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-50/'+getFile +'.jpg')
-    //             break
-    //         case "4": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-100/'+getFile +'.jpg')
-    //             break
-    //         case "5": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-500/'+getFile +'.jpg')
-    //             break
-    //         case "6": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-1000/'+getFile +'.jpg')
-    //             break
-    //     }
-
-        
-    // });
-
-    
-    // $('#submit').click(function(){ 
-    //     let getFile = Math.floor(1+ Math.random() * 82)
-
-    //     switch ($('#difficulty :selected').val()) {
-    //         case "1":
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-5/'+getFile +'.jpg')
-    //             break;
-    //         case "2": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-25/'+getFile +'.jpg')
-    //             break
-    //         case "3": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-50/'+getFile +'.jpg')
-    //             break
-    //         case "4": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-100/'+getFile +'.jpg')
-    //             break
-    //         case "5": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-500/'+getFile +'.jpg')
-    //             break
-    //         case "6": 
-    //             $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //             $(".genImg").attr("src",'./assets/img/stable-100/'+getFile +'.jpg')
-    //             break
-    //         // default:
-    //         //     $(".humImg").attr("src",'./assets/img/laion/'+getFile +'.jpg')
-    //         //     $(".genImg").attr("src",'./assets/img/stable-25/'+getFile +'.jpg')
-    //         //     break;
-    //     }
-        
-
-    // });
-
-       
-
-    jQuery(':button').click(function () {
-        $(':button').prop('disabled', true);
-        if (this.id == 'genOpt1') {
-            if(correctAnswer = "top"){
-                $(this).css("border","10px solid red");      
-            }
-            else{
-                $(this).css("border","10px solid red");    
-            }
-        }
-        else if (this.id == 'genOpt2') {
-            if(correctAnswer = "bottom"){
-                $(this).css("border","10px solid red");      
-            }
-            else{
-                $(this).css("border","10px solid red");    
-            }
-        }
-
-        else if (this.id == 'realOpt1') {
-            if(correctAnswer = "top"){
-                $(this).css("border","10px solid green");      
-            }
-            else{
-                $(this).css("border","10px solid red");    
-            }
-        }
-        else if (this.id == 'realOpt2') {
-            if(correctAnswer = "bottom"){
-                $(this).css("border","10px solid green");      
-            }
-            else{
-                $(this).css("border","10px solid red");    
-            }
-        }
-        else if(this.id == "continue"){
-            location.reload();
-
-
-        }
-
-        $('#continue').prop('disabled', false);
-
-        
-
-    });
-
-
-    
-    // $('#genOpt1, #genOpt2', '#realOpt1','#realOpt2').click(function () {
-    //     if (this.id == 'submit1') {
-    //        alert('Submit 1 clicked');
-    //     }
-    //     else if (this.id == 'submit2') {
-    //        alert('Submit 2 clicked');
-    //     }
-    //  });
-
-    
+    allFiles=jsonData
+    getRandFile(allFiles)
 });
 
 
+}
+
+fetch('./assets/js/img-info.json')
+    .then(data => data.json())
+    .then(jsonFiles => initialize(jsonFiles));
 
 
 
 
 
-// 	$("button").click(async function () {
-// 		// await response
-// 		let user = await getData();
 
-// 		// because we use "await" ^ we know that data was returned successfully
-// 		console.log(user);
-// 		$(".randomUser").html(`
-// 		<img src="${user.picture.large}"><br>
-// 		Name: ${user.name.first} ${user.name.last}<br>
-// 		Country: ${user.location.country}<br>
-// 		Age: ${user.dob.age}<br>
-// 		Password: <code>${user.login.password}</code>
-// 	`);
-// 	});
-// 	$("button").trigger("click");
-// });
+
+
+

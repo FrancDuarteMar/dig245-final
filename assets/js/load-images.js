c5fc80cef9d953ac0f81597336219c5ef7ed54a3
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-11-29 13:36:44 -0500

    Added pagination specific changes

--- assets/js/load-images.js
+++ assets/js/load-images.js
@@ -1,9 +1,26 @@
 function initialize(jsonData){   
     $(document).ready(function () {
+        console.log("There are " + Object.keys(jsonData).length + " Elements")
+        var currPageVal = 0
+            // $('<ul></ul>').addClass("pagination").attr("id","page-"+currPageVal).appendTo($("#images"))
+            // for(var key in jsonData){
+        // $('<ul></ul>').addClass("pagination").attr("id","page-"+currPageVal).appendTo($("#images"))
 
-        for(var key in jsonData){
-            $("<div></div>").addClass("row").addClass("imageLine").attr("id","image-"+key).appendTo("#images")
+        // for (let i = 1; i < Object.keys(jsonData).length+1; i++) {
+        for (let i = 1; i <= 3; i++) {
 
+            
+            // if((i-1)%3 === 0){
+            // currPageVal ++
+            
+            // $('<hr>').appendTo("#images")
+
+            // }
+
+            key = String(i)
+            $("<li></li>").addClass("page-item").attr("id","li-"+key).appendTo($("#page-"+currPageVal))
+            
+            $("<div></div>").addClass("row").addClass("imageLine").attr("id","image-"+key).appendTo("#images").appendTo($('#page-'+currPageVal)).appendTo($("#li-"+key))
 
             $('<img/>').attr('src','./assets/img/laion/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
@@ -18,25 +35,20 @@ function initialize(jsonData){
             $('<img/>').attr('src','./assets/img/stable-500/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
             
             $('<img/>').attr('src','./assets/img/stable-1000/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
-
-            $('<hr>').appendTo("#images")
+              
         }
 
+        
+          
         window.addEventListener('load', function () {
             Lightense('img:not(.no-lightense),.lightense');
           }, false);
-          
-
        
 
 
     
     });
 }
-
-
-
-
 
 fetch('./assets/js/img-info.json')
     .then(data => data.json())

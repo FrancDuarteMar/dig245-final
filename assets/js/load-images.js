cf9e9dddccafbe4706462d370dc3375c0ed6aaff
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-11-29 20:23:00 -0500

    Added prompt and updated background for image zoom

--- assets/js/load-images.js
+++ assets/js/load-images.js
@@ -1,26 +1,33 @@
 var currPageNum;
 
+var imageZoomBack = "rgba(0,0,0,0.05)"
+// var imageZoomBack = `lightblue url("./assets/img/question.jpg") no-repeat fixed center`
+// var imageZoomBack = ""
+
 function loadMore(jsonData){
     for (let i = currPageNum; i < currPageNum+3 && i <= 82; i++) {
     
         key = String(i)
 
+            $("<div></div>").addClass("row prompt-row").attr("id","prompt-"+key).appendTo("#images")
+            $("#prompt-"+key).html(`<h6 class=" prompt"> Prompt: `+jsonData[key]["prompt"]+"</h6>")
+            
             $("<div></div>").addClass("row").addClass("imageLine").attr("id","image-"+key).appendTo("#images")
 
 
-            $('<img/>').attr('src','./assets/img/laion/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/laion/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-5/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-5/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-25/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-25/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-50/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-50/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-100/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-100/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-500/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-500/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
             
-            $('<img/>').attr('src','./assets/img/stable-1000/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-1000/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
             $('<hr>').appendTo("#images")
             
@@ -37,22 +44,25 @@ function initialize(jsonData){
 
             key = String(i)
 
+            $("<div></div>").addClass("row prompt").attr("id","prompt-"+key).appendTo("#images")
+            $("#prompt-"+key).html("<h6> Prompt: "+jsonData[key]["prompt"]+"</h6>")
+
             $("<div></div>").addClass("row").addClass("imageLine").attr("id","image-"+key).appendTo("#images")
 
 
-            $('<img/>').attr('src','./assets/img/laion/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/laion/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-5/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-5/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-25/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-25/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-50/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-50/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-100/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-100/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
-            $('<img/>').attr('src','./assets/img/stable-500/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-500/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
             
-            $('<img/>').attr('src','./assets/img/stable-1000/'+jsonData[key]["image"]).addClass("image-elem").appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
+            $('<img/>').attr('src','./assets/img/stable-1000/'+jsonData[key]["image"]).addClass("image-elem").attr("data-lightense-background",imageZoomBack).appendTo($("<div></div>").addClass("col col-md-1").appendTo("#image-"+key));
 
             $('<hr>').appendTo("#images")
             Lightense('img:not(.no-lightense),.lightense');

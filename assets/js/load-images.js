cf101e5060d73152dcc5a7471355817769830e29
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-12-04 01:42:37 -0500

    Added MD bootstrap and CSS to home page

--- assets/js/load-images.js
+++ assets/js/load-images.js
@@ -1,15 +1,15 @@
 var currPageNum;
-
+const imagesToLoad = 3;
 var imageZoomBack = "rgba(0,0,0,0.05)"
 // var imageZoomBack = `lightblue url("./assets/img/question.jpg") no-repeat fixed center`
 // var imageZoomBack = ""
 
 function loadMore(jsonData){
-    for (let i = currPageNum; i < currPageNum+3 && i <= 82; i++) {
+    for (let i = currPageNum; i < currPageNum+imagesToLoad && i <= 82; i++) {
     
         key = String(i)
 
-            $("<div></div>").addClass("row prompt-row").attr("id","prompt-"+key).appendTo("#images")
+            $("<div></div>").addClass("col prompt-row col-xl-8 offset-xl-2").attr("id","prompt-"+key).appendTo("#images")
             $("#prompt-"+key).html(`<h6 class=" prompt"> Prompt: `+jsonData[key]["prompt"]+"</h6>")
             
             $("<div></div>").addClass("row").addClass("imageLine").attr("id","image-"+key).appendTo("#images")
@@ -38,14 +38,11 @@ function loadMore(jsonData){
 function initialize(jsonData){   
     $(document).ready(function () {
 
-        
-     
-        for (let i = 1; i <= 3 && i <= 82; i++) {
+        for (let i = 1; i <= imagesToLoad && i <= 82; i++) {
 
             key = String(i)
-
-            $("<div></div>").addClass("row prompt").attr("id","prompt-"+key).appendTo("#images")
-            $("#prompt-"+key).html("<h6> Prompt: "+jsonData[key]["prompt"]+"</h6>")
+            $("<div></div>").addClass("col prompt-row col-xl-8 offset-xl-2").attr("id","prompt-"+key).appendTo("#images")
+            $("#prompt-"+key).html(`<h6 class=" prompt"> Prompt: `+jsonData[key]["prompt"]+"</h6>")
 
             $("<div></div>").addClass("row").addClass("imageLine").attr("id","image-"+key).appendTo("#images")
 
@@ -69,18 +66,18 @@ function initialize(jsonData){
 
 
         }
-        currPageNum = 4
+        currPageNum = imagesToLoad+1
 
         $('#load-more-btn').bind("click",function(){
             loadMore(jsonData)
             Lightense('img:not(.no-lightense),.lightense');
 
-            if(currPageNum + 4 >=82){
+            if(currPageNum + imagesToLoad >=82){
                 $("#load-more-btn").html("Reached the end.")
                 $("#load-more-btn").prop('disabled', true);
             } 
             console.log("currently on page load num: "+     currPageNum)
-            currPageNum += 4 
+            currPageNum += imagesToLoad+1
         });
 
     

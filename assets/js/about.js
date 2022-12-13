02b9077e714d9ec0fffc3416ffcd04d69abe3f18
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-12-13 15:45:57 -0500

    Pushed main text in about and fixed unicode issues

--- assets/js/about.js
+++ assets/js/about.js
@@ -4,36 +4,36 @@ $(document).ready(function () {
         if(this.id == "about-data-btn"){
             if($(this).attr("value") == "ON"){
                 $(this).attr("value","OFF")
-                $("#about-icon").html("⮟")
+                $("#about-icon").html("&#9660")
                 $("#row-about").hide()
             }
             else{
                 $(this).attr("value","ON")
-                $("#about-icon").html("⮝")
+                $("#about-icon").html("&#9650")
                 $("#row-about").show()
             }
         }
         else if(this.id == "ai-art-btn"){
             if($(this).attr("value") == "ON"){
                 $(this).attr("value","OFF")
-                $("#ai-icon").html("⮟")
+                $("#ai-icon").html("&#9660")
                 $("#row-ai").hide()
             }
             else{
                 $(this).attr("value","ON")
-                $("#ai-icon").html("⮝")
+                $("#ai-icon").html("&#9650")
                 $("#row-ai").show()
             }
         }
         else if(this.id == "class-btn"){
             if($(this).attr("value") == "ON"){
                 $(this).attr("value","OFF")
-                $("#class-icon").html("⮟")
+                $("#class-icon").html("&#9660")
                 $("#row-class").hide()
             }
             else{
                 $(this).attr("value","ON")
-                $("#class-icon").html("⮝")
+                $("#class-icon").html("&#9650")
                 $("#row-class").show()
             }
         }
